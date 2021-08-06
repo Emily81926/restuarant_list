827860d6a2bcfe990a1f7708ffc649f3d3afad96
Emily81926 <emily81926@gmail.com>
2021-08-06 17:34:56 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -30,7 +30,30 @@ app.get('/', (req, res) => {
   Restaurant.find()
             .lean()
             .then( stores => res.render('index',{stores}))
-            .catch(error =>console.log(error))
+            .catch(error => console.log(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+
+})
+
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  console.log('req.body.phone', req.body.phone)
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+
 })
 
 app.get('/restaurants/:id', (req, res) => {
@@ -42,6 +65,7 @@ app.get('/restaurants/:id', (req, res) => {
  
 })
 
+
 // app.get('/search', (req, res) => {
 
 //   const keyword = req.query.keyword
@@ -51,11 +75,20 @@ app.get('/restaurants/:id', (req, res) => {
 //   res.render('index', { items: restaurant, keyword: keyword })
 // })
 
-app.get('/restaurants/new',(req, res)=>{
-  return res.render('new')
+
+
+
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((item) => res.render('edit', { item }))
+    .catch(error => console.log(error))
 })
 
-app.post('/restaurants', (req, res) =>{
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
   const name = req.body.name
   const name_en = req.body.name_en
   const category = req.body.category
@@ -65,14 +98,22 @@ app.post('/restaurants', (req, res) =>{
   const google_map = req.body.google_map
   const rating = req.body.rating
   const description = req.body.description
-  
-  console.log('req.body.phone', req.body.phone)
-  return Restaurant.create({ name , name_en , category , image , location , phone , google_map ,  rating , description })
-                 .then(() => res.redirect('/'))
-                 .catch(error => console.log(error))
-
+  return Restaurant.findById(id)
+      .then(item => {
+        item.name = name,
+        item.name_en = name_en,
+        item.category = category,
+        item.image = image,
+        item.location = location,
+        item.phone = phone,
+        item.google_map = google_map,
+        item.rating = rating,
+        item.description = description
+        return item.save()
+      })
+      .then(() => res.redirect(`/restaurants/${id}`))
+      .catch(error => console.log(error))
 })
-
 
 app.listen(port, () => {
   console.log(`express is listening on http://localhost:${port}`)
