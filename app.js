3e6340b93cc9a702153cbe4fa7554626ba2ddaba
Emily81926 <emily81926@gmail.com>
2021-08-06 17:49:45 +0800

    feat: add Restaurant delete function

--- app.js
+++ app.js
@@ -115,6 +115,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
       .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete',(req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+        .then(item => item.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`express is listening on http://localhost:${port}`)
 })
