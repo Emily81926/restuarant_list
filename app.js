16cf93b19eb7b492066f1ce9220d6532948e4c61
Emily81926 <emily81926@gmail.com>
2021-10-11 22:54:32 +0800

    feat: add facebook login and dotenv

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const restaurant = require('./models/restaurant.js')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 
--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,19 @@ const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 const{ authenticator } = require('../middleware/auth')
 
 // 將網址結構符合/字串的request導向home模組
 router.use('/restaurants', authenticator, restaurants)
 
+router.use('/auth', auth)
+
+router.use('/users', users)
+
 router.use('/sort', authenticator, sort)
 
 router.use('/search', authenticator, search)
-
-router.use('/users', users)
 
 router.use('/', authenticator ,home)
 
