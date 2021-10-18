32350fbfd0ea8099c4af95ae027ebf397edece31
Emily81926 <emily81926@gmail.com>
2021-10-18 21:14:34 +0800

    feat:modify search feature

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,11 +4,20 @@ const Restaurant = require('../../models/restaurant')
 
 
 router.get('/', (req, res) => {
-  const keyword = req.query.keyword
-  const keywordRegex = new RegExp(keyword, 'i');
+  const userId = req.user._id
+  const keyword = req.query.keyword.trim().toLowerCase()
+  let stores = []
 
-  Restaurant.find({ $or: [{ category: { $regex: keywordRegex } }, { name: { $regex: keywordRegex } }] })
+  Restaurant.find({ userId })
     .lean()
+    .then(restaurants => {
+      stores = restaurants.filter(data => {
+        const dataName = data.name.trim().toLowerCase()
+        const dataCategory = data.category.trim().toLowerCase()
+        return (dataName.includes(keyword) || dataCategory.includes(keyword))
+      })
+       return stores
+    })
     .then(stores => res.render('index', { stores, keyword }))
     .catch(error => console.log(error))
 
